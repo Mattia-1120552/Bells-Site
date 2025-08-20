795c550124e46d9408844f79e9daedac03dfe2d6
Belljahh <mattiaabrahamwarning@gmail.com>
2025-08-20 21:17:28 +0200

    small

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,54 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
 import { RouterView } from 'vue-router'
 import { ref } from 'vue'
 import WelcomeHeader from './components/WelcomeHeader.vue'
 import RootSideBar from './components/SideBar/RootSideBar.vue'
 
-const route = useRoute()
 const visible = ref(false)
 </script>
 
 <template>
-  <div class="backgroundcolorApp">
+  <div class="background"></div>
+  <div class="outerlayer h-[100vh]">
     <header>
-      <div
-        class="absolute top-0 left-0 w-full h-1 bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 z-30"
-      ></div>
-
       <header
         class="theme-color opacity-30 absolute top-0 left-0 w-[100vw] flex justify-center items-center h-[75px] hover:opacity-100 transition duration-1000 z-20 bg-black"
       >
         <WelcomeHeader />
       </header>
       <div
-        class="w-full h-30 md:h-70 bg-cover bg-center z-[-1]"
-        style="background-image: url('/banner.jpg')"
+        class="w-full h-20 bg-cover bg-center z-[-1]"
+        style="background-image: url('/banner1.jpg')"
       ></div>
     </header>
 
-    <div
-      class="absolute w-full h-2 z-30 bg-gradient-to-b from-indigo-500/100 via-pink-500/50 to-purple-500/20"
-    ></div>
-
-    <div class="theme-color View p-[1.5rem] flex justify-center bg-black">
-      <h1>{{ route.meta.title }}View</h1>
+    <div class="theme-color View justify-center bg-black">
+      <!--Waves Container-->
+      <div>
+        <svg
+          class="waves"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          viewBox="0 24 150 28"
+          preserveAspectRatio="none"
+          shape-rendering="auto"
+        >
+          <defs>
+            <path
+              id="gentle-wave"
+              d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+            />
+          </defs>
+          <g class="parallax">
+            <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255, 105, 180, 0.7)" />
+            <!-- Hot pink -->
+            <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255, 182, 193, 0.5)" />
+            <!-- Light pink -->
+            <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255, 192, 203, 0.3)" />
+          </g>
+        </svg>
+      </div>
+      <!--Waves end-->
     </div>
 
     <div class="p-10 px-[20rem] w-[100vw] h-[66vh]">
