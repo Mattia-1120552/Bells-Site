7864da88011d1105c5ca9b87bbf02239e7628bb6
Belljahh <mattiaabrahamwarning@gmail.com>
2025-08-22 23:53:30 +0200

    small font size

--- src/components/WelcomeHeader.vue
+++ src/components/WelcomeHeader.vue
@@ -9,7 +9,7 @@ const route = useRoute()
     class="view theme-color opacity-50 absolute top-0 left-0 w-[100vw] flex justify-center items-center h-[75px] hover:opacity-100 transition duration-1000 z-20 bg-black"
     style="background-position: center; background-size: cover"
   >
-    <h1>{{ route.meta.title }}</h1>
+    <div class="text-[18px] sm:text-[25px]">{{ route.meta.title }}</div>
   </header>
   <div class="w-full h-20 bg-cover bg-center z-[-1]"></div>
 </template>
